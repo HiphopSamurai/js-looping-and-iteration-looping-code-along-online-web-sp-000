ac38f7632334a1424ffdff0f07be9fb2cf65969b
Garrett Thompson <gtcruz.gt@gmail.com>
2020-05-15 05:18:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfnames, event) {
+  for (let i = 0; i < writeCards.length; i++) {
+    console.log('Thank you ${arrayOfnames[i]}, for the wonderful ${event} gift!)';
+  }
+}
