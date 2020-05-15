81bcacb3db47e60de0bfdd2bc975d740088de7b4
Garrett Thompson <gtcruz.gt@gmail.com>
2020-05-15 05:27:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function writeCards(arrayOfnames, event) {
   var newArray = []
   for (let i = 0; i < arrayOfnames.length; i++) {
     //console.log('Thank you, ' + arrayOfnames[i] + ", for the wonderful " + event + " gift!");
-    push.newArray = 'Thank you, ' + arrayOfnames[i] + ", for the wonderful " + event + " gift!";
+    push.newArray = console.log('Thank you, ' + arrayOfnames[i] + ", for the wonderful " + event + " gift!");
   }
 
   return newArray;
